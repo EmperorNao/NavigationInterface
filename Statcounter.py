d93a4c550435f43d1b7dd793ead91fc3735aeb36
EmperorNao <59420696+EmperorNao@users.noreply.github.com>
2021-10-27 21:31:24 +0300

    Create Statcounter.py

---
+++ Statcounter.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+class NavigationSignalReceiver:
+
+    def __init__(self, values):
+        self.values = None
+
+    def set_values(self, values):
+        self.values = values
+
+    def count(self):
+        pass
+
+    def get_corr_matrix(self):
+        pass
+
+    def get_cov_matrix(self):
+        pass
+
+    def get_Q(self):
+        pass
+
